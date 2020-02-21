7e2d1178b7732942773d190325e1c06b9336d91b
Jorin G <jgedri@gmail.com>
2020-02-21 10:28:55 -0500

    Add project files.

---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+module Person =
+    type T = {First:string; Last:string}
+
+        // Constructor
+       let create first last =
+        {First=first; Last=last}
+
+        // standalone function
+    let fullName {First=first; last=last} =
+         first + " " last
